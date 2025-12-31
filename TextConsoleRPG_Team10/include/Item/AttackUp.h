fc1e8a883a2595ec9ab654bbbd6cd17a59b844f8
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-31 10:15:53 +0900

    한글꺠짐 테스트

--- TextConsoleRPG_Team10/include/Item/AttackUp.h
+++ TextConsoleRPG_Team10/include/Item/AttackUp.h
@@ -4,7 +4,9 @@
 class AttackUp : public IItem
 {
 public:
-    //생성자 - 공격력 증가 효과 아이템 설정 (이름, 금액, 증가 수치, 보유 가능 개수)
+    //생성자
+    //한글 깨짐 테스트
+    //공격력 증가 효과 아이템 설정 (이름, 금액, 증가 수치, 보유 가능 개수)
     AttackUp() : IItem("공격력 증가 포션", 100, 10, 5) {};
     void ApplyEffect(Player& p) override;
 };
